b808ba6f00745ac570c52ccaea42a76d738c54bc
jkernagis2 <jkernagis2@gmail.com>
2016-07-27 16:42:08 -0600

    Update for mutliple slideables via class
    Updated to allow selecting elements by "slideable" CSS class instead of single element ID.  Also updated to allow slideable div to be expanded by default by setting expanded="true" attribute.  Tweaks to get slideable divs to completely hide and restore initial state properly.

--- angularSlideables.js
+++ angularSlideables.js
@@ -7,17 +7,27 @@ angular.module('angularSlideables', [])
             var contents = element.html();
             element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
 
+            var cssObj = {
+                'transitionProperty': 'height'
+            };
+
+            if(attr.expanded === "true" || attr.expanded === true){
+                cssObj.overflowY = "scroll";
+                cssObj.height = "100%";
+                cssObj.display = "inline";
+            }else{
+                cssObj.overflowY = "hidden";
+                cssObj.height = "0px";
+                cssObj.display = "block";
+            }
+
             return function postLink(scope, element, attrs) {
                 // default properties
                 attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
                 attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
-                element.css({
-                    'overflow': 'hidden',
-                    'height': '0px',
-                    'transitionProperty': 'height',
-                    'transitionDuration': attrs.duration,
-                    'transitionTimingFunction': attrs.easing
-                });
+                cssObj.transitionDuration = attrs.duration;
+                cssObj.transitionTimingFunction = attrs.easing;
+                element.css(cssObj);
             };
         }
     };
@@ -27,22 +37,28 @@ angular.module('angularSlideables', [])
         restrict: 'A',
         link: function(scope, element, attrs) {
             var target, content;
-            
-            attrs.expanded = false;
-            
+
             element.bind('click', function() {
-                if (!target) target = document.querySelector(attrs.slideToggle);
-                if (!content) content = target.querySelector('.slideable_content');
-                
-                if(!attrs.expanded) {
-                    content.style.border = '1px solid rgba(0,0,0,0)';
-                    var y = content.clientHeight;
-                    content.style.border = 0;
-                    target.style.height = y + 'px';
-                } else {
-                    target.style.height = '0px';
+                if (!target) target = document.querySelectorAll(attrs.slideToggle);
+                for(var i = 0; i < target.length; i++){
+                    if (!content) content = target[i].querySelector('.slideable_content');
+
+                    if(target[i].getAttribute("expanded") === "true" || target[i].getAttribute("expanded") === true){
+                        target[i].style.height = '0px';
+                        target[i].style.overflowY = "hidden";
+                        target[i].style.display = 'block';
+                        target[i].setAttribute("expanded",false);
+                    }else{
+                        content.style.border = '1px solid rgba(0,0,0,0)';
+                        var y = content.clientHeight;
+                        content.style.border = 0;
+                        target[i].style.display = "inline";
+                        target[i].style.overflowY = "scroll";
+                        target[i].style.height = '100%';
+                        target[i].setAttribute("expanded",true);
+                    }
+
                 }
-                attrs.expanded = !attrs.expanded;
             });
         }
     }
